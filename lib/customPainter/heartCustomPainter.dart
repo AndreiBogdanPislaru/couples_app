f7ce3eefc05865d92a222f2135321ba6bac637ea
Orsetto <andreibogdan.pislaru@gmail.com>
2024-07-16 00:56:08 +0200

    [v 0.0.3b3] Tested an earcut algorithm to fulfil the heart, it works partially

--- lib/customPainter/heartCustomPainter.dart
+++ lib/customPainter/heartCustomPainter.dart
@@ -1,8 +1,30 @@
 import 'dart:math';
 import 'dart:ui';
 
+import 'package:dart_earcut/dart_earcut.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+List<double> convertListOfOffsetsInArray({required List<Offset> listOfOffsets}) {
+  List<double> convertedList = [];
+
+  for (Offset element in listOfOffsets) {
+    convertedList.add(element.dx);
+    convertedList.add(element.dy);
+  }
+
+  return convertedList;
+}
+
+List<Offset> convertListOfPositionsInListOfOffset({required List<int> listOfPosition, required List<Offset> offsetList}) {
+  List<Offset> result = [];
+
+  for (int i = 0; i < listOfPosition.length; i++) {
+    result.add(offsetList[listOfPosition[i]]);
+  }
+
+  return result;
+}
 
 class HeartCustomPainter extends CustomPainter {
   @override
@@ -15,16 +37,23 @@ class HeartCustomPainter extends CustomPainter {
       double x = 7 * pow(sin(rad), 3) as double;
       double y = (10 * cos(rad) - 3 * cos(2 * rad) - 2 * cos(3 * rad) - cos(4 * rad) );
 
-      points.add(Offset(x, y));
-      canvas.drawPoints(PointMode.points, [Offset(x * 20, y * -20)], Paint()..color = Colors.red
-      ..strokeWidth = 7);
+      points.add(Offset(25 * x, 15 * y));
     }
 
     //canvas.drawPoints(PointMode.points, points, Paint()..color = Colors.red);
     canvas.save();
-    canvas.rotate(45 * (pi / 180));
-    canvas.drawRect(Rect.fromLTRB(0, 0, 100, 200), Paint()..color = Colors.green);
-    canvas.drawPoints(PointMode.lines, points, Paint()..color = Colors.red);
+    canvas.rotate(180 * (pi / 180));
+    //canvas.drawRect(Rect.fromLTRB(0, 0, 100, 200), Paint()..color = Colors.green);
+    List<double> listOfPoints = convertListOfOffsetsInArray(listOfOffsets: points);
+    List<int> triangles = Earcut.triangulateRaw(listOfPoints);
+    List<Offset> polygonOffset = convertListOfPositionsInListOfOffset(listOfPosition: triangles, offsetList: points);
+
+    Vertices vertices = Vertices(VertexMode.triangleStrip, polygonOffset);
+    canvas.drawVertices(vertices, BlendMode.color, Paint()..color = Colors.yellow);
+    canvas.drawPoints(PointMode.polygon, points,
+        Paint()
+          ..color = Colors.red
+          ..strokeWidth = 3);
     //canvas.rotate(45 * (pi / 180));
     canvas.restore();
 
